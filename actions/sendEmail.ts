8e4d68e33c9ee6fbd8970d50b99a3c5c0cd11ddf
Ben-Sicat <bensicat00@gmail.com>
2024-04-10 04:33:08 +0800

    recover

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -5,7 +5,7 @@ import { Resend } from "resend";
 import { validateString, getErrorMessage } from "@/lib/utils";
 import ContactFormEmail from "@/email/contact-form-email";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend('re_TAjgNHh2_5ASLTdFkv2mKd4jAFz5zzjGD');
 
 export const sendEmail = async (formData: FormData) => {
   const senderEmail = formData.get("senderEmail");
