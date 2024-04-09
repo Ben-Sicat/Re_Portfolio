09e2dd925e7185ea405e07025dd7fab3aac9b7a1
Ben-Sicat <bensicat00@gmail.com>
2024-04-10 03:47:18 +0800

    recover

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ import { Toaster } from "react-hot-toast";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Ricardo | Personal Portfolio",
-  description: "Ricardo is a full-stack developer with 8 years of experience.",
+  title: "Ben | Personal Portfolio",
+  description: "Ben is a Software Dev.",
 };
 
 export default function RootLayout({
